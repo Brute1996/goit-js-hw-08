ecfbd98325eff8d5536abb356b890a5bff1f691b
Sergey <sergey.cherenkov96@gmail.com>
2022-12-03 23:57:39 +0200

    fix 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,5 +9,9 @@ player.on('timeupdate', throttle((e) => {
     localStorage.setItem('videoplayer-current-time', e.seconds)
 }, 1000));
 
+const secondsPlayed = localStorage.getItem('videoplayer-current-time');
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+if (secondsPlayed) {
+    player.setCurrentTime(secondsPlayed)
+}
+
